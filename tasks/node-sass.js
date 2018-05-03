02786f69364e3300b1a0d7443f86ff7cd2537106
Mark Higham <mark.higham@gmail.com>
2018-05-03 11:11:48 +0100

    added some basic sass options

--- tasks/node-sass.js
+++ tasks/node-sass.js
@@ -1,43 +1,53 @@
-const path = require('path');
-const fs = require('fs-extra');
-const sass = require('node-sass');
+const path = require("path");
+const fs = require("fs-extra");
+const sass = require("node-sass");
 
-module.exports = function (grunt) {
-    grunt.registerMultiTask('sass', 'compile sass', function () {
-        let done = this.async(),
-            promises;
+module.exports = function(grunt) {
+  grunt.registerMultiTask("sass", "compile sass", function() {
+    let done = this.async(),
+      promises;
 
-        promises = this.filesSrc.map((item) => {
-            return new Promise((resolve, reject) => {
-                sass.render({
-                    file: item
-                }, (err, result) =>{
-                    if (err) {
-                        reject(err);
-                    } else {
-                        let dir = path.dirname(this.data.dest);
-
-                        if (!fs.existsSync(dir)) {
-                            fs.mkdirpSync(dir);
-                        }
-
-                        fs.writeFile(this.data.dest, result.css.toString(), (err) => {
-                            if (err) {
-                                reject(err);
-                            } else {
-                                resolve();
-                            }
-                        });
-                    }
-                })
-            });
-        });
-
-        Promise.all(promises)
-            .then(() => done())
-            .catch((err) => {
-                console.log(err);
-                done(true);
-            })
+    const options = this.options({
+      outputStyle: "nested",
+      sourceComments: false,
     });
+
+    promises = this.filesSrc.map(item => {
+      return new Promise((resolve, reject) => {
+        sass.render(
+          {
+            file: item,
+            outputStyle: options.outputStyle,
+            sourceComments: options.sourceComments,
+          },
+          (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              let dir = path.dirname(this.data.dest);
+
+              if (!fs.existsSync(dir)) {
+                fs.mkdirpSync(dir);
+              }
+
+              fs.writeFile(this.data.dest, result.css.toString(), err => {
+                if (err) {
+                  reject(err);
+                } else {
+                  resolve();
+                }
+              });
+            }
+          },
+        );
+      });
+    });
+
+    Promise.all(promises)
+      .then(() => done())
+      .catch(err => {
+        console.log(err);
+        done(true);
+      });
+  });
 };
